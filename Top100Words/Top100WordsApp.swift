f31cbd09b26a609cdd946c6ec2da62a2f1f13dbb
John Giorgi <johnmgiorgi@gmail.com>
2023-04-03 19:04:13 -0400

    Initial Commit

---
+++ Top100Words/Top100WordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Top100WordsApp.swift
+//  Top100Words
+//
+//  Created by John Giorgi on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct Top100WordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
